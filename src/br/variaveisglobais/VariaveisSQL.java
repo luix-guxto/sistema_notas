f56ee2f2440539b1f28c914221b325d9474344c7
Luis Augusto <luisaugustoway@gmail.com>
2022-12-05 16:25:23 -0300

    up to date

---
+++ src/br/variaveisglobais/VariaveisSQL.java
@@ -0,0 +1,10 @@
+package br.variaveisglobais;
+
+final public class VariaveisSQL {
+    public final static String
+            USERNAME = "username",
+            PASSWORD = "password",
+            DATABASE = "database",
+            HOST = "host";
+
+}
